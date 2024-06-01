d95465cbfe7af56fb39c29acdea149182323482e
Your name <samuel.pascale@01s.it>
2024-06-01 19:19:19 +0200

    try new style

--- src/Game.js
+++ src/Game.js
@@ -257,22 +257,22 @@ const Game = ({ columns, rows }) => {
           <p>Phase: {phase}</p>
           <p>Sum: {score}</p>
         </div>
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center h-screen pt-4">
           <div
-            className={`grid gap-1`}
+            className={`grid border-2 border-gray-400`}
             style={{
-              gridTemplateColumns: `repeat(${columns}, minmax(15px, 50px))`,
+              gridTemplateColumns: `repeat(${columns}, minmax(15px, 40px))`,
             }}
           >
             {grid.map((num, index) => (
               <div
                 key={index}
-                className={`flex justify-center w-full items-center border border-gray-300 cursor-pointer select-none text-lg font-semibold 
+                className={`flex justify-center w-full bg-white items-center border border-gray-300 cursor-pointer select-none text-lg font-semibold 
             ${
               selected.includes(num?.id)
                 ? wrongSelection
                   ? "bg-red-300 hover:bg-red-300"
-                  : "bg-yellow-300 hover:bg-yellow-300"
+                  : "bg-blue-300 hover:bg-blue-300"
                 : ""
             }
             ${num.id !== -1 ? "hover:border-2 hover:bg-gray-50" : ""}
@@ -280,8 +280,8 @@ const Game = ({ columns, rows }) => {
           `}
                 style={{
                   height: "auto",
-                  minHeight: "50px",
-                  maxHeight: "calc(50px / columns)",
+                  minHeight: "40px",
+                  maxHeight: "calc(40px / columns)",
                 }}
                 onClick={() => handleSelect(num?.id)}
               >
